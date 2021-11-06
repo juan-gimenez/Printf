f2fc37ddfc466f7c0459235dd8ad800a896151cf
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-06 13:12:10 -0700

    documented

--- print_string.c
+++ print_string.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include "main.h"
-
+/**
+ * print_string - prints a string
+ *Return: 0 
+ *@s: string
+ */
 void print_string(char *s);
 {
 	write(1, s, (sizeof(char) * _strlen(s));
