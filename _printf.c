a4b54f47f362dc51a22c0c5beb141f4892646690
Diekkan <merentiel.diego@gmail.com>
2021-11-08 04:43:56 -0800

    update

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *
@@ -7,37 +6,6 @@
 
 int _printf(const char *format, ...)
 {
-	va_list ad;
-	unsigned int i, j = 0;
-	formatprints formats[] = {
-		{"%", NULL},
-        	{"c", print_char},
-        	{"s", print_string},
-        	{NULL, NULL}
-	};
-
-	va_start(ad, format);
+	va_list ap;
+	unsigned int i;
 	
-	while (format != NULL && format[j] != '\0')
-	{
-		i = 0;
-			if (formats[i].lf[0] != format[j])
-			{
-				_putchar(format[j]);
-			}
-			else
-			{	
-				j++;
-				while (format[j] != formats[i].lf[0] && formats[i].lf[0] != 0)
-				{
-					i++;
-					if (format[j] == formats[i].lf[0])
-						formats[i].f(ad);
-				}
-			}
-		j++;
-	}
-
-	va_end(ad);
-	return (0);
-}
