b2e6dd53cb0275ea054736870413a1eaf3bf8fcd
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-08 09:44:37 -0800

    added border cases

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - our own printf.
@@ -8,39 +9,48 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ad;
-	unsigned int count = 0;
-	unsigned int i = 0;
-
-	va_start(ad, format);
-
-	for (; format[i] != 0; i++)
-	{
+  va_list ad;
+  unsigned int count = 0;
+  unsigned int i = 0;
+  
+  if (format)
+    {
+	    va_start(ad, format);
+	    if (ad == NULL)
+	      return (-1);
+	    for (; format[i] != 0; i++)
+	      {
 		if (format[i] != '%')
-		{
-			_putchar(format[i]);
+		  {
+		    _putchar(format[i]);
 			count++;
-		}
+		  }
 		else
-		{
-			i++;
+		  {
+		    i++;
 			switch (format[i])
-			{
-			case 'c':
-				print_char(ad);
-				count++;
-				break;
-			case 's':
-				count += print_string(ad);
-				break;
-			case 'd':
-				count += print_decimal();
-			default:
-				_putchar(format[i]);
-			}
-		}
+			  {
+			  case 'c':
+			    print_char(ad);
+			    count++;
+			    break;
+			  case 's':
+			    count += print_string(ad);
+			    break;
+			  case 'd':
+			    count += print_decimal();
+			  default:
+			  _putchar(format[i]);
+			  }
+		  }
 	}
-	printf("%d\n", count);
-	printf("%i\n", count);
-	return (count);
+	
+	
+	  }
+}
+
+}
+printf("%d\n", count);
+printf("%i\n", count);
+return (count);
 }
