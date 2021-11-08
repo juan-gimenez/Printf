f8b344b9a70f1953068d35f8efdcf43551c98968
Juan Gimenez <juancitogimenez0@gmail.com>
2021-11-08 08:30:45 -0800

    rm

--- test/2-main.c
+++ test/2-main.c
@@ -1,4 +1,5 @@
-#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -7,6 +8,7 @@
  */
 int main(void)
 {
-    print_all("ceis", 'B', 3, "stSchool");
+  int i = 34;
+  printf("%d\n", i);
     return (0);
 }
