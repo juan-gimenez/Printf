c09c4b11887183e7f3e281c75c714612d4b87bfe
Diekkan <merentiel.diego@gmail.com>
2021-11-08 10:02:15 -0800

    updates

--- _printf.c
+++ _printf.c
@@ -6,56 +6,46 @@
  *@format: format specified
  * ... - arguments passed
  *Return: number of bytes printed1
-
  */
+
 int _printf(const char *format, ...)
 {
-  va_list ad;
-  unsigned int count = 0;
-  unsigned int i = 0;
-  
-if (format)
-    {
-      va_start(ad, format);
-      if (ad == NULL)
-	return (-1);
-      for (; format[i] != 0; i++)
+	va_list ad;
+	unsigned int count = 0;
+	unsigned int i = 0;
+
+	if (format)
 	{
-	  if (format[i] != '%')
-	    {
-	      _putchar(format[i]);
-	      count++;
-		  }
-	  else
-	    {
-	      i++;
-	      switch (format[i])
-		
+		va_start(ad, format);
+		if (ad == NULL)
+			return (-1);
+		for (; format[i] != 0; i++)
 		{
-		case 'c':
-		  print_char(ad);
-		  count++;
-				break;
-		case 's':
-		  count += print_string(ad);
-		  break;
-			case 'd':
-			  count += print_decimal(ad);
-			  break;
-		default:
-			 count += _putchar(format[i]);
+			if (format[i] != '%')
+			{
+				_putchar(format[i]);
+				count++;
+			}
+			else
+			{
+				i++;
+				switch (format[i])
+				{
+					case 'c':
+						print_char(ad);
+						count++;
+						break;
+					case 's':
+						count += print_string(ad);
+						break;
+					case 'd':
+						count += print_decimal(ad);
+						break;
+					default:
+						count += _putchar(format[i]);
+				}
+			}
 		}
-	    }
-	  else
-	    return(-1);
 	}
-    }
-}
-
-
-	
-
-printf("%d\n", count);
-printf("%i\n", count);
-return (count);
+	return (count);
 }
