19f0c3ef366e743c45674982e45da9f925ff9f6b
Diekkan <merentiel.diego@gmail.com>
2021-11-08 11:13:46 -0800

    updates

--- main.h
+++ main.h
@@ -2,20 +2,8 @@
 #define _HEADER_
 #define MY_BUFFER_SIZE 1024
 #include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-
-/**
- *
- */
-
-typedef struct all
-{
-	char *lf;
-	void (*f)(va_list ap);
-
-} formatprints;
 
 int _printf(const char *format, ...);
 void print_char(va_list ad);
