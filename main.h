dc595306fb67f1d8e3a8dd86070d537423226b11
Diekkan <merentiel.diego@gmail.com>
2021-11-08 16:02:54 -0800

    update

--- main.h
+++ main.h
@@ -8,7 +8,6 @@ int _printf(const char *format, ...);
 int print_char(va_list ad);
 int print_string(va_list ad);
 int print_decimal(va_list ad);
-int _strlen(const char *s);
 int _putchar(char c);
 
 #endif
