a8313db8b7de6efc3d14cd5e3491507ccc7c68f9
Diekkan <merentiel.diego@gmail.com>
2021-11-08 19:05:45 -0800

    updates

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 			return (-1);
 		if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 			return (-1);
-		for (; format[i] != 0 && format; i++)
+		for (; format[i] != '\0' && format != NULL; i++)
 		{
 			if (format[i] != '%')
 			{
